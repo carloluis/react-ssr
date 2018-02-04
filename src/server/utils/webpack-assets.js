0f25952225812472d61ac451b07b8a4a83ac1c50
carloluis <carloluisr@gmail.com>
2018-02-03 23:52:44 -0300

    page assets filtering..

--- src/server/utils/webpack-assets.js
+++ src/server/utils/webpack-assets.js
@@ -1,7 +1,7 @@
 const webpackStats = require('../../../dist/webpack-stats.json');
 
 function getAssets() {
-	return webpackStats.assets.map(asset => asset.name);
+	return webpackStats.assets.map(asset => asset.name).filter(asset => /(.js|.css)$/.test(asset));
 }
 
 module.exports = getAssets;
